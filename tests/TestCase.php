7da323e213647c0b8dad3113733a915bad1c3e6a
Arya Dian <aryadian003@gmail.com>
2026-02-03 15:57:52 +0700

    feat: Configure PHPUnit to use an in-memory SQLite database for tests and establish a dedicated testing environment.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,19 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        // Force testing environment
+        putenv('APP_ENV=testing');
+        $_ENV['APP_ENV'] = 'testing';
+        $_SERVER['APP_ENV'] = 'testing';
+
+        // Manually load .env.testing
+        if (file_exists(__DIR__.'/../.env.testing')) {
+            $dotenv = \Dotenv\Dotenv::createMutable(__DIR__.'/../', '.env.testing');
+            $dotenv->load();
+        }
+        
+        parent::setUp();
+    }
 }
